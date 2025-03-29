f4e3256806a8d543b6ea433a4ee5cc8b5996b9f1
mangsuyo <mangsuyo3610@gmail.com>
2025-03-29 21:26:24 +0900

    deploy: 관리자 웹 페이지 배포 (#1)

--- src/repository/API.ts
+++ src/repository/API.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 export const API = axios.create({
-  baseURL: "/api",
+  baseURL,
   headers: { "Content-Type": "application/json" },
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://buddyya.store",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        ws: true,
-      },
-    },
-  },
+  server: {},
 });
